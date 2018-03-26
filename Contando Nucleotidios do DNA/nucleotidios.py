3b88d4ee6d743a150dea191707210ba2cbb6442d
rafaela <rafaela.micaela.77@gmail.com>
2018-03-26 20:12:35 -0300

    :stuck_out_tongue_closed_eyes:ajeitei a formatação

--- Contando Nucleotidios do DNA/nucleotidios.py
+++ Contando Nucleotidios do DNA/nucleotidios.py
@@ -3,10 +3,10 @@ with open("rosalind_dna.txt") as f:
 dna = int()
 
 print(" Rosalind DNA ")
-print("Quantidade de Amostras do DNA: %d" % len(dna))
-print("Quantidade de Adenina: %d" % dna.cout('A'))
-print("Quantidade de Timina: %d" % dna.cout('T'))
-print("Quantidade de Guanina: %d" % dna.cout('G'))
-print("Quantidade de Citosina: %d" % dna.cout('C'))
+print("Quantidade de Amostras do DNA: " '{:d}'.format(len(dna)))
+print("Quantidade de Adenina: "'{:d}'.format(dna.cout('A')))
+print("Quantidade de Timina: "'{:d}'.format(dna.cout('T')))
+print("Quantidade de Guanina: " '{:d}'.format(dna.cout('G')))
+print("Quantidade de Citosina: "'{:d}'.format(dna.cout('C')))
 
 f.close()
