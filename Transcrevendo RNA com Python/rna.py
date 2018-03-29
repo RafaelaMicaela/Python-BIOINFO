e140f9bf3a56b389047e2cbdd0eca83aa6ea64d1
rafaela <rafaela.micaela.77@gmail.com>
2018-03-29 16:03:58 -0300

    :boom:

--- Transcrevendo RNA com Python/rna.py
+++ Transcrevendo RNA com Python/rna.py
@@ -1,22 +1,10 @@
-with open("rosalind_rna.txt") as f:
-	read_data = f.read()
-rna = int()
-print(" Rosalind RNA")
-print("Quantidade de Amostras de DNA: "'{:d}'.format(len(rna)))
-print("Quantidade de Adenina: " '{:d}'.format(rna.count('A')))
-print("Quantidade de Timina: " '{:d}'.format(rna.count('T')))
-print("Quantidade de Guanina: "'{:d}'.format(rna.count('G')))
-print("Quantidade de Citosina: "'{:d}'.format(rna.count('C')))
+def transcribe_rna(t):
+    return t.replace('T','U')
 
-print("-----------------------------------------------")
-print("Substituindo amostras de Timina por Uracila...")
-print("-----------------------------------------------")
 
-rna = rna.replace('T', 'U')
-print("Quantidade de Amostras de RNA: "'{:d}'.format(len(rna)))
-print("Quantidade de Adenina: "'{:d}'.format(rna.count('A')))
-print("Quantidade de Uracila: "'{:d}'.format(rna.count('U')))
-print("Quantidade de Guanina: " '{:d}'.format(rna.count('G')))
-print("Quantidade de Citosina: "'{:d}'.format(rna.count('C')))
+if __name__ == "__main__":
 
-f.close()
+    small_dataset = "GATGGAACTTGACTACGTAAATT"
+    large_dataset = "GAACTAAGGGCTGGGTTATGCTCCCTAGTTCCAACCTGCGGCTAAGAATCATTAGGTAGATAGTGCGGTAAGGGTGTAACATTACCGCCTTTAATCTTCTCTCCGTAGGGTTAAGGCTGGGTTAAGAGTACGACTATGATCCAGGGCGACTGATGGGTGTTTCGGGGACGCCCTGTTGAGCAAGGCTGAATTAATGCGAGTGTAACTTAGTATGTCGCAACTCGCTGTGCCATCCGTAAGGCGTTACGCACAGGTTATGCATCTTGCACGGCCTCTATGGAGAGACTTGTCACGTTATTTATGACAACACTGGGGCAGATATACCCTCACCGAGTGCCCGCCCTGTTCACTACAACCATCGACGCGATCTCGAACTTGATACTAACTCTGGGCGACGTACAAGGTCGGGGTCACACCGCCCGGGGCATACATTAGTGAGAGCTAACCCACATATTTCATTGGTGTCCCGATACTCCGCCGCTATTATGTATAATTTGTCACACCGCTGGCCTAGACCACACCACACACTGACTTAATGCTTGCACCTCTCCTAAAACAAGTGCACATGAGGGTAATATCGCGACTTTCTTCATGAACATCATCGATGTCACATCTCAATAGTGCTTCTGAATCGCTAGCCTCTGGTCGGAGGATACTGATAAGGCGCTTAACCAAGGCATTCAGTGAGTTAGCGGGTCGTCTTGCGCGCCAATCCCTTACAAATTTTTCTGGCTTAGCCCTGAAGCCTTCTCTACATATAGGGGTATGTGAGTCGCAGGTAAGCCTTCTTCTAGGGTAAGGTCAGACAATCTGTGTATTATATCGTAAGGTAGGCAAGCCCGTTGCTTCGTGCCTTTATCATTGACATATTCAAAATTGGCGGGACTCAAGCTTGCGTGGAC"
+
+    print transcribe_rna(large_dataset)
